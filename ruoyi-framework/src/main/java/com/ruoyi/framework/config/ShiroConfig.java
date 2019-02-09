abcb80d43e12c454f0c073545d96b8a46a0037ee
zgf <chriszh123@sina.com>
2019-02-09 17:59:49 +0800

    client接口访问去shiro过滤

--- ruoyi-framework/src/main/java/com/ruoyi/framework/config/ShiroConfig.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/ShiroConfig.java
@@ -282,6 +282,16 @@ public class ShiroConfig
         filterChainDefinitionMap.put("/logout", "logout");
         // 不需要拦截的访问
         filterChainDefinitionMap.put("/login", "anon,captchaValidate");
+        // 客户端访问不需要拦截
+        filterChainDefinitionMap.put("/fac/banner/**", "anon");
+        filterChainDefinitionMap.put("/fac/config/**", "anon");
+        filterChainDefinitionMap.put("/fac/discounts/**", "anon");
+        filterChainDefinitionMap.put("/fac/notice/**", "anon");
+        filterChainDefinitionMap.put("/fac/order/**", "anon");
+        filterChainDefinitionMap.put("/fac/score/**", "anon");
+        filterChainDefinitionMap.put("/fac/shop/**", "anon");
+        filterChainDefinitionMap.put("/fac/template-msg/**", "anon");
+        filterChainDefinitionMap.put("/fac/user/**", "anon");
         // 系统权限列表
         // filterChainDefinitionMap.putAll(SpringUtils.getBean(IMenuService.class).selectPermsAll());
 
