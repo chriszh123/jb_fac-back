cc67c2bac6b1512ae391924c0266bf6471e6c132
zhangguifeng <01117498@wisedu.com>
2019-08-21 19:40:11 +0800

    调整砍价算法，匹配砍价金额范围

--- ruoyi-fac/src/main/java/com/ruoyi/fac/util/KJAlgorithmUtil.java
+++ ruoyi-fac/src/main/java/com/ruoyi/fac/util/KJAlgorithmUtil.java
@@ -9,7 +9,7 @@ public class KJAlgorithmUtil {
      * 1.总金额不超过总共可砍的价格*100  单位是分
      * 2.每次砍价都能砍到金额，最低不能低于1分，最大金额不能超过（总共可砍的价）*100
      */
-    private static final int MINMONEY = 1;
+    private static int MINMONEY = 1;
     private static int MAXMONEY = 0;
 
     /**
@@ -39,7 +39,6 @@ public class KJAlgorithmUtil {
     /**
      * 随机分配一个金额
      *
-     * @param mnoney    mnoney
      * @param minS：最小金额
      * @param maxS：最大金额
      * @param count
@@ -70,29 +69,31 @@ public class KJAlgorithmUtil {
     /**
      * 砍价
      *
-     * @param money 可砍总价 : 分
-     * @param price 底价：分
-     * @param count 个数 : 砍价过程 多少次砍价可以结束
+     * @param toCutMoney 待砍金额 : 分
+     * @param minMoney   砍价范围min：分
+     * @param minMoney   砍价范围max：分
+     * @param count      个数 : 砍价过程 多少次砍价可以结束
      * @return
      */
-    public static List<Double> splitReducePrice(int money, int price, int count) {
-        MAXMONEY = price;
+    public static List<Double> splitReducePrice(int toCutMoney, int minMoney, int maxMoney, int count) {
+        MAXMONEY = maxMoney;
+        MINMONEY = minMoney;
         //红包合法性分析
-        if (!isRight(money, count)) {
+        if (!isRight(toCutMoney, count)) {
             return new ArrayList<>();
         }
         //红包列表
         List<Double> list = new ArrayList<>();
         //每个红包的最大的金额为平均金额的TIMES倍
-        int max = (int) (money * TIMES / count);
+        int max = (int) (toCutMoney * TIMES / count);
         max = max > MAXMONEY ? MAXMONEY : max;
         //分配红包
         int sum = 0;
 
         for (int i = 0; i < count; i++) {
-            int one = randomReducePrice(money, MINMONEY, max, count - i);
+            int one = randomReducePrice(toCutMoney, MINMONEY, max, count - i);
             list.add(one / 100.0);
-            money -= one;
+            toCutMoney -= one;
             sum += one;
         }
         System.out.println("sum:" + sum);
@@ -100,7 +101,7 @@ public class KJAlgorithmUtil {
     }
 
     public static void main(String[] args) {
-        List<Double> list = splitReducePrice(3900, 200 * 100, 9);
+        List<Double> list = splitReducePrice(3900, 220, 3000, 11);
         System.out.println(list);
     }
 }
