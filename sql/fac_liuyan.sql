3af950ac897f9a068cb46aa20f98807ff4b0d91c
zhangguifeng <01117498@wisedu.com>
2019-11-22 17:27:29 +0800

    createPoster：整理代码

--- sql/fac_liuyan.sql
+++ sql/fac_liuyan.sql
@@ -15,3 +15,19 @@ CREATE TABLE `fac_leave_message` (
   `is_deleted` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否删除',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='用户留言表';
+
+drop table if exists `fac_leave_message_reply`;
+CREATE TABLE `fac_leave_message_reply` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `token_ask` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '留言用户token ',
+  `token_answer` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '留言用户token ',
+  `ask_id` varchar(2048) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT '用户留言',
+  `mngt_remark` varchar(2048) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT '留言回复',
+
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL COMMENT '最近更新时间',
+  `operator_id` bigint(20) DEFAULT NULL COMMENT '操作者ID',
+  `operator_name` varchar(60) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '操作者姓名',
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否删除',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='用户留言回复表';
